0bf8b080bf76c6f90e928e5bf30bb284d139491b
golcmanKA <golcmanck@gmail.com>
2026-04-14 14:08:58 +0300

    Create_Models

---
+++ GoldenCrown/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace GoldenCrown.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }
+
+        public int ReceiverId { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public decimal Amoutn { get; set; }
+    }
+}
---
+++ GoldenCrown/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+
+namespace GoldenCrown.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set;  }
+
+        public string Name { get; set;  }
+
+        public string Password { get; set;  }
+    }
+}
